ddcefc4e7dbfe689fdbd66c629114a9111f45aad
Wendell Eduardo Albuquerque Bonucci <contact.wendellbonucci@gmail.com>
2026-02-24 21:13:31 -0300

    Criação da função do loanding
    Criei a função de loanding para carregar os componentes primeiro e depois carregar o conteúdo do site.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,34 @@
+"use client"
+import { useState, useEffect } from "react";
 import styles from "./page.module.css";
 import InitialContent from "@/components/InitialContent/InitialContent";
 import About from "@/components/About/About";
+import Loading from "@/utils/loading/Loading";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setLoading(false);
+    };
+
+    if (document.readyState === "complete") {
+      setLoading(false);
+    } else {
+      window.addEventListener("load", handleLoad);
+      return () => window.removeEventListener("load", handleLoad);
+    }
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <div className={styles.page}>
       <InitialContent />
       <About />
     </div>
-  );
+  );  
 }
